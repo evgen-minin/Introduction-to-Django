01d2f2ab6785c57f8f1f0ff17ca8af69f6bcade2
Евгений Минин <evgenminin1988@mail.ru>
2023-07-23 13:03:33 +0300

    task 4

--- catalog/templatetags/media_tags.py
+++ catalog/templatetags/media_tags.py
@@ -5,8 +5,17 @@ from urllib.parse import urljoin
 register = template.Library()
 
 
-@register.simple_tag
-def mediapath(image):
+@register.filter
+def media_path(image):
     if image:
-        return urljoin(settings.MEDIA_URL, str(image))
+        return urljoin(settings.MEDIA_URL, image.url)
+    return ""
+
+
+@register.simple_tag
+def media_path(image):
+    if image:
+        if not isinstance(image.url, str):
+            raise TypeError("image.url is not a string")
+        return urljoin(str(settings.MEDIA_URL), str(image.url))
     return ""
