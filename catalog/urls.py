14a225fca89ffb47e54ba235817358a256a3a7e3
Евгений Минин <evgenminin1988@mail.ru>
2023-07-23 19:24:37 +0300

    task 4

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import card_product, home_controller, contact_controller, create_product
-from config import settings
 
 urlpatterns = [
     path('', home_controller, name='home'),
@@ -10,5 +8,3 @@ urlpatterns = [
     path('catalog/', card_product, name='catalog'),
     path('create/', create_product, name='create_product')
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
